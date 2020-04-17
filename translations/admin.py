2c82b60075f8ce63ac29bd93cd681f0c3e2bd730
Aniruddha Adhikary <aniruddha@saleswhale.com>
2020-04-17 14:08:55 +0800

    Add reversion

--- translations/admin.py
+++ translations/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
+from reversion.admin import VersionAdmin
+
 from .models import Language, Category, Phrase, Translation
 
 
 @admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
     list_display = ['name', 'native_name', 'code']
+    search_fields = ['name', 'native_name', 'code']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+
+class TranslationInlineAdmin(admin.StackedInline):
+    model = Translation
+    fields = ['language', 'content', 'special_note']
+    extra = 0
+
+
+@admin.register(Phrase)
+class PhraseAdmin(VersionAdmin):
+    list_display = ['summary', 'category', 'updated_at']
+    search_fields = ['summary', 'content']
+    inlines = [TranslationInlineAdmin]
--- translations/models.py
+++ translations/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -9,12 +10,18 @@ class Language(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Phrase(models.Model):
@@ -25,7 +32,22 @@ class Phrase(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.summary
 
-class Translation(Phrase):
+
+class Translation(models.Model):
+    phrase = models.ForeignKey('Phrase', on_delete=models.CASCADE)
+
     language = models.ForeignKey('Language', on_delete=models.CASCADE)
+    content = models.TextField()
     special_note = models.TextField(verbose_name='Special note to doctor (if needed)', blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.phrase.summary} for {self.language.name}'
+
+    class Meta:
+        unique_together = ('language', 'phrase')
