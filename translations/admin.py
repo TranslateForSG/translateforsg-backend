adaac92b81f2286c240ab0a69d729653e4c34dfe
Aniruddha Adhikary <aniruddha@saleswhale.com>
2020-04-17 13:50:42 +0800

    start adding admin

--- translations/admin.py
+++ translations/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Language, Category, Phrase, Translation
 
-# Register your models here.
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'native_name', 'code']
--- translations/models.py
+++ translations/models.py
@@ -6,15 +6,24 @@ class Language(models.Model):
     native_name = models.CharField(max_length=100)
     code = models.CharField(max_length=10, db_index=True, unique=True)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Phrase(models.Model):
     summary = models.CharField(max_length=100)
     content = models.TextField()
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Translation(Phrase):
