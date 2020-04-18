c97820e6c0c26306848fae2547ff385a72aaa916
Aniruddha Adhikary <aniruddha@saleswhale.com>
2020-04-18 12:54:59 +0800

    translations: reorganize codebase

---
+++ translations/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+
+from translations.models import Translation, Language, Phrase, Category, Volunteer
+
+
+class TranslationSerializer(serializers.ModelSerializer):
+    language = serializers.StringRelatedField()
+    category = serializers.StringRelatedField()
+
+    class Meta:
+        model = Translation
+        fields = ['language', 'category', 'content', 'special_note', 'audio_clip']
+
+
+class PhraseSerializer(serializers.ModelSerializer):
+    translations = TranslationSerializer(many=True, source='translation_set')
+    category = serializers.StringRelatedField()
+
+    class Meta:
+        model = Phrase
+        fields = ['summary', 'content', 'category', 'translations']
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = ['name', 'native_name', 'code']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+
+class VolunteerSerializer(serializers.ModelSerializer):
+    language = serializers.StringRelatedField()
+
+    class Meta:
+        model = Volunteer
+        fields = ['display_name', 'language', 'phone_number', 'availability', 'notes']
--- translations/views.py
+++ translations/views.py
@@ -1,25 +1,8 @@
-from rest_framework import mixins, viewsets, serializers, filters
+from rest_framework import mixins, viewsets, filters
 
 from translateforsg.pagination import PerPage100
-from translations.models import Translation, Language, Phrase, Category, Volunteer
-
-
-class TranslationSerializer(serializers.ModelSerializer):
-    language = serializers.StringRelatedField()
-    category = serializers.StringRelatedField()
-
-    class Meta:
-        model = Translation
-        fields = ['language', 'category', 'content', 'special_note', 'audio_clip']
-
-
-class PhraseSerializer(serializers.ModelSerializer):
-    translations = TranslationSerializer(many=True, source='translation_set')
-    category = serializers.StringRelatedField()
-
-    class Meta:
-        model = Phrase
-        fields = ['summary', 'content', 'category', 'translations']
+from translations.models import Language, Phrase, Category, Volunteer
+from translations.serializers import PhraseSerializer, LanguageSerializer, CategorySerializer, VolunteerSerializer
 
 
 class PhraseViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -30,22 +13,10 @@ class PhraseViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     search_fields = ['summary', 'content']
 
 
-class LanguageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Language
-        fields = ['name', 'native_name', 'code']
-
-
 class LanguageViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = LanguageSerializer
     queryset = Language.objects.all().order_by('name')
     pagination_class = PerPage100
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['name']
 
 
 class CategoryViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -54,15 +25,7 @@ class CategoryViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     pagination_class = PerPage100
 
 
-class VolunteerSerializer(serializers.ModelSerializer):
-    language = serializers.StringRelatedField()
-
-    class Meta:
-        model = Volunteer
-        fields = ['display_name', 'language', 'phone_number', 'availability', 'notes']
-
-
 class VolunteerViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = VolunteerSerializer
-    queryset = Volunteer.objects.all().order_by('?') # make sure to randomly order
+    queryset = Volunteer.objects.all().order_by('?')  # make sure to randomly order
     pagination_class = PerPage100
