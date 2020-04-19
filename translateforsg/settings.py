ff278efe8a7c28536c3f5feb3353550f8c636544
Aniruddha Adhikary <aniruddha@saleswhale.com>
2020-04-19 11:07:34 +0800

    fix env loading

--- translateforsg/settings.py
+++ translateforsg/settings.py
@@ -14,14 +14,14 @@ import os
 import environ
 
 env = environ.Env(
-    # set casting, default value
     DEBUG=(bool, False)
 )
-# reading .env file
-environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# reading .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
