1a0039170bd44302ed8625c2c902945910eac348
Aniruddha Adhikary <aniruddha@saleswhale.com>
2020-04-26 16:52:51 +0800

    allow filtering by phrase_ids

--- translations/filters.py
+++ translations/filters.py
@@ -6,7 +6,15 @@ from translations.models import Translation
 class TranslationFilterSet(django_filters.FilterSet):
     phrase__category__name = django_filters.CharFilter(field_name='phrase__categories__name', lookup_expr='iexact')
     lookup = django_filters.CharFilter(field_name='phrase__content', lookup_expr='iexact')
+    phrases = django_filters.CharFilter(method='filter_phrases')
 
     class Meta:
         model = Translation
         fields = ['language__name', 'phrase__categories', 'phrase__categories__name']
+
+    def filter_phrases(self, queryset, name, value: str):
+        try:
+            ids = map(int, value.split(','))
+            return queryset.filter(phrase_id__in=ids)
+        except ValueError:
+            return queryset
